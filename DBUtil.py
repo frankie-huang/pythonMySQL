e450f3b6d6aa19cd73b54d7ffe73c1f4327f1c3a
anybus <chaixiaohui@sf-express.com>
2018-03-02 18:45:39 +0800

    update

--- DBUtil.py
+++ DBUtil.py
@@ -1,6 +1,7 @@
 from pythonMySQL import *
 import csv
 
+
 class CRUD_DAO(object):
     def __init__(self, table_name, newConfig=0):
         self.cur_table = table_name
@@ -11,12 +12,12 @@ class CRUD_DAO(object):
         return res
 
     def addOne(self, data):
-        id = self.dao.add(data)
+        id = self.dao.add(data)  # 返回lastID,如果pk 为字符串，返回0
         return id
 
-    def save(self, data):
+    def saveById(self, data):
         '''
-        根据id查找更新
+        根据pk查找更新,pk为空，报错
         :param data:
         :return:
         '''
@@ -25,6 +26,10 @@ class CRUD_DAO(object):
 
     def updateOneField(self, field_name, field_val, save_where={}):
         res = self.dao.table(self.cur_table).where(save_where).setField(field_name, field_val)  # 返回更新影响行数
+        return res
+
+    def updateFields(self, f_v_dict, save_where={}):
+        res = self.dao.table(self.cur_table).where(save_where).setField(f_v_dict)  # 返回更新影响行数
         return res
 
     def deleteById(self, primary_key_value):
@@ -39,9 +44,15 @@ class CRUD_DAO(object):
         res = self.dao.where(where_param).delete()
         return res
 
+    def printDebugSql(self):
+        print(self.dao.getLastSql())
+
+    def printSql(self):
+        print(self.dao._sql())
+
 
 class UserDAO(CRUD_DAO):
-    def __init__(self, table_name, newConfig=0):
+    def __init__(self, table_name='t_user', newConfig=0):
         super(UserDAO, self).__init__(table_name, newConfig)
 
     def findByName(self, name):
@@ -50,27 +61,56 @@ class UserDAO(CRUD_DAO):
         res = self.dao.where(where_param).select()
         return res
 
+    def findDetailinfoById(self, id):
+        '''
+        内连接,
+        :param id:
+        :return:
+        '''
+        field = {
+            'a.id': '',
+            'a.name': 'name',
+            'a.age': 'age',
+            'b.nickname': 'nickname',
+            'b.lastlogintime': 'lastlogintime'
+        }
+        where_param = {}
+        where_param['a.id'] = id
+        res = self.dao.table('t_user as a,chat_user as b').field(field).where('a.id = b.id').where(where_param).select()
+        return res
+
 
 class PoiTypeDAO(CRUD_DAO):
     def __init__(self, table_name, newConfig=0):
         super(PoiTypeDAO, self).__init__(table_name, newConfig)
 
-    # def findAllPoitype(self):
-
+    def findAllPoitype(self):
+        where_param = {}
+        where_param['level'] = 3
+        res = self.dao.where(where_param).select()
+        return res
 
 
 if __name__ == '__main__':
-    poitypeDao = PoiTypeDAO('poi_type')
-    f = r'D:\work\03\poitype\poitype.csv'
-    inlist = []
-    with open(f, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for e in reader:
-            inlist.append(e)
-        csvfile.close()
-    print(len(inlist))
-    for e in inlist:
-        poitypeDao.addOne(e)
+    userDao = UserDAO()
+    res = userDao.findDetailinfoById(11)
+    userDao.printDebugSql()
+    print(res)
+    # poitypeDao = PoiTypeDAO('poi_type')
+    # res = poitypeDao.findById('01')
+    # poitypeDao.printDebugSql()
+    # poitypeDao.printSql()
+    # print(len(res))
+    # f = r'D:\work\03\poitype\poitype1.csv'
+    # inlist = []
+    # with open(f, newline='') as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for e in reader:
+    #         inlist.append(e)
+    #     csvfile.close()
+    # print(len(inlist))
+    # for e in inlist:
+    #     poitypeDao.addOne(e)
     # userDao = UserDAO('t_user')
     # res = userDao.findByName('jim')
     # print(res)
