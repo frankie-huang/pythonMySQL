38b2a13d8e20f2ebae9879cdfe8dda97de80d31c
anybus <chaixiaohui@sf-express.com>
2018-03-05 14:26:18 +0800

    123

--- DBUtil.py
+++ DBUtil.py
@@ -1,5 +1,4 @@
 from pythonMySQL import *
-import csv
 
 
 class CRUD_DAO(object):
@@ -51,6 +50,8 @@ class CRUD_DAO(object):
         print(self.dao._sql())
 
 
+
+
 class UserDAO(CRUD_DAO):
     def __init__(self, table_name='t_user', newConfig=0):
         super(UserDAO, self).__init__(table_name, newConfig)
@@ -79,74 +80,68 @@ class UserDAO(CRUD_DAO):
         res = self.dao.table('t_user as a,chat_user as b').field(field).where('a.id = b.id').where(where_param).select()
         return res
 
+'''
+user表
+CREATE TABLE t_user(
+id int(20) AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) not null ,
+age int(3)
+)ENGINE = INNODB DEFAULT CHARSET=utf8
 
-class PoiTypeDAO(CRUD_DAO):
-    def __init__(self, table_name, newConfig=0):
-        super(PoiTypeDAO, self).__init__(table_name, newConfig)
+chat表
+CREATE TABLE chat_users (
+id INT(20) AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) not null ,
+nickname VARCHAR(50) not null,
+lastlogintie datetime
+)ENGINE = INNODB DEFAULT CHARSET=utf8
 
-    def findAllPoitype(self):
-        where_param = {}
-        where_param['level'] = 3
-        res = self.dao.where(where_param).select()
-        return res
-
+'''
 
 if __name__ == '__main__':
     userDao = UserDAO()
+    # userDao = UserDAO('t_user') 封装UserDao后
+    '''
+    findbyName
+    return userlist
+    '''
+    res = userDao.findByName('jim')
+    print(res)
+
+    '''
+    与chat表关联查询
+    return list
+    '''
     res = userDao.findDetailinfoById(11)
     userDao.printDebugSql()
+    print('the result is: ',res)
+
+    '''
+    插入一条
+    :return id
+    '''
+    data = {
+        'name':'jim',
+        'age':10
+    }
+    res = userDao.addOne(data)
     print(res)
-    # poitypeDao = PoiTypeDAO('poi_type')
-    # res = poitypeDao.findById('01')
-    # poitypeDao.printDebugSql()
-    # poitypeDao.printSql()
-    # print(len(res))
-    # f = r'D:\work\03\poitype\poitype1.csv'
-    # inlist = []
-    # with open(f, newline='') as csvfile:
-    #     reader = csv.DictReader(csvfile)
-    #     for e in reader:
-    #         inlist.append(e)
-    #     csvfile.close()
-    # print(len(inlist))
-    # for e in inlist:
-    #     poitypeDao.addOne(e)
-    # userDao = UserDAO('t_user')
-    # res = userDao.findByName('jim')
-    # print(res)
-    # poitype_dao = CRUD_DAO('poi_type')
-    # res = poitype_dao.deleteById('02')
-    # print(res)
-    # data = {
-    #     'code': '02',
-    #     'name': '汽车服务',
-    #     'name_en': 'Auto Service',
-    #     'primary_code': '',
-    #     'level': 1
-    # }
-    # res = poitype_dao.addOne(data)
-    # print(res)
-    # user_dao = CRUD_DAO('t_user')
-    # data = {
-    #     'name':'jim',
-    #     'age':10
-    # }
-    # res = user_dao.addOne(data)
-    # print(res)
-    # data = {
-    #     'name': 'tom',
-    #     'age': 10
-    # }
-    # res = user_dao.addOne(data)
-    # print(res)
-    # res = user_dao.deleteById(3)
-    # print(res)
-    # res = user_dao.deleteByIds([9, 10])
-    # print(res)
-    # data = {
-    #     'id': 11,
-    #     'name': 'jim',
-    #     'age': 100
-    # }
-    # res = user_dao.save(data)
-    # print(res)
+
+    '''
+    根据id列表删除对应数据
+    :return 影响条数
+    '''
+    res = userDao.deleteByIds([9, 10])
+    print(res)
+
+    '''
+    根据id，更新数据
+    :return 影响条数
+    '''
+    data = {
+        'id': 11,
+        'name': 'jim',
+        'age': 100
+    }
+    res = userDao.save(data)
+    print(res)
